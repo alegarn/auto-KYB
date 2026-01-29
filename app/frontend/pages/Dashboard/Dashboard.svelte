38599ac91f01f0941ed329a34af454a78cc5da9c
alegarn <alexgarnier78310@protonmail.com>
2026-01-29 09:52:59 +0100

    feat: add session path functionality to Dashboard and update tsconfig for routes

--- app/frontend/pages/Dashboard/Dashboard.svelte
+++ app/frontend/pages/Dashboard/Dashboard.svelte
@@ -2,6 +2,8 @@
   import * as Sidebar from "/components/ui/sidebar/index.js";
   import AppSidebar from "/components/customs/app-sidebar.svelte";
   import { page } from '@inertiajs/svelte'
+  import { session_path } from "/routes/index.js";
+
   console.log($page.props) // inspect keys sent by server
 
   let { children, user, session_id } = $props();
@@ -16,5 +18,6 @@
   {@render children?.()}
   <p> Hello {user?.email}</p>
   <p> Session ID: {session_id}</p>
+  <p> Session Path: {session_path(session_id)}</p>
  </main>
 </Sidebar.Provider>
