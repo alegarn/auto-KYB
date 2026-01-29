a3f18220b73df58583ae604351e58b2ac843bc0a
alegarn <alexgarnier78310@protonmail.com>
2026-01-29 16:05:00 +0100

    feat: remove authentication callback and unused method from DashboardController

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,4 @@
-class DashboardController < ApplicationController
-  before_action :authenticate
-  
+class DashboardController < ApplicationController  
   def index
     render inertia: "Dashboard/Dashboard", props: {
       user: current_user,
@@ -13,11 +11,7 @@ class DashboardController < ApplicationController
   def current_user
     Current.session&.user
   end
-
-  def iser_signed_in?
-    Current.session.present?
-  end
-
+  
   def current_session_id
     Current.session&.id
   end
