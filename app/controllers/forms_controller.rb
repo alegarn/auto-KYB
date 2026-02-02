69fc12489c916745cdf208fa4dcdfac98684a48b
alegarn <alexgarnier78310@protonmail.com>
2026-02-02 16:42:03 +0100

    feat: implement form serialization and helper; enhance forms controller for improved response handling

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -1,19 +1,13 @@
 class FormsController < ApplicationController
   def index
-    forms = current_user ? current_user.forms.order(created_at: :desc).map { |f| { id: f.id, name: f.name, created_at: f.created_at.to_date.to_s } } : []
+    forms = if current_user
+      FormSerializer.collection(current_user.forms.order(created_at: :desc))
+    else
+      []
+    end
 
-    if Rails.env.test?
-      # simple HTML fallback for feature specs that don't render Inertia
-      html = <<~HTML
-        <h1>My Forms</h1>
-        <p>#{ERB::Util.html_escape(current_user&.email.to_s)}</p>
-        <p><a href="/forms/new">New Form</a></p>
-        <ul>
-          #{forms.map { |f| "<li>#{ERB::Util.html_escape(f[:name])}</li>" }.join}
-        </ul>
-      HTML
-
-      render html: html.html_safe
+    if request.format.html?
+      render :index, locals: { forms: forms, user: current_user }
     else
       render inertia: "Forms/Index", props: {
         user: current_user,
@@ -25,26 +19,12 @@ class FormsController < ApplicationController
 
   def show
     form = current_user.forms.find(params[:id])
-    render json: form.as_json(include: :form_fields)
+    render json: form.as_json(only: [:id, :name, :description], include: { form_fields: { only: [:id, :label, :field_type, :required, :position] } })
   end
 
   def new
-    if Rails.env.test?
-      html = <<~HTML
-        <h1>Create Form</h1>
-        <form action="/forms" method="post">
-          <label for="name">Name</label>
-          <input id="name" name="form[name]" />
-
-          <label for="field_label_1">Field 1 label</label>
-          <input id="field_label_1" name="form[structure][fields][][label]" />
-
-          <input type="submit" value="Create" />
-          <a href="/forms">Cancel</a>
-        </form>
-      HTML
-
-      render html: html.html_safe
+    if request.format.html?
+      render :new, locals: { user: current_user }
     else
       render inertia: "Forms/New", props: { user: current_user }
     end
@@ -53,16 +33,12 @@ class FormsController < ApplicationController
   def create
     # convert permitted params to plain hash for service layer
     created = FormService.create_form(current_user, form_params.to_h)
-    if Rails.env.test?
-      redirect_to "/forms"
-    else
-      redirect_to forms_path
-    end
+    redirect_to forms_path
   rescue ActiveRecord::RecordInvalid => e
-    if Rails.env.test?
-      render plain: e.record.errors.full_messages.join(','), status: :unprocessable_entity
+    if request.format.html?
+      render :new, locals: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
     else
-      render inertia: "Forms/New", props: { errors: e.record.errors.full_messages }
+      render inertia: "Forms/New", props: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
@@ -77,6 +53,6 @@ class FormsController < ApplicationController
   end
 
   def form_params
-    params.require(:form).permit(:name, :description, structure: [ fields: [:label, :field_type, :required, :position, metadata: {}] ])
+    params.require(:form).permit(:name, :description, structure: { fields: [:label, :field_type, :required, :position, metadata: {}] })
   end
 end
