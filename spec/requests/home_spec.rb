4429aa89fafcec874c2c296f1dcebe6b0c958213
alegarn <alexgarnier78310@protonmail.com>
2026-02-02 09:27:49 +0100

    test: enhance form request specs and add redirection check for unauthenticated users

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -6,6 +6,11 @@ RSpec.describe "Homes", type: :request do
       get "/"
       expect(response).to have_http_status(:success)
     end
-  end
 
+    it "renders the Home/Index Inertia component" do
+      get "/"
+      expect(response.headers["X-Inertia"]).to eq("true")
+      expect(response.parsed_body["component"]).to eq("Home/Index")
+    end
+  end
 end
