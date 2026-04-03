4332eb35ef03af2c263e74179031f89d59abc2bb
alegarn <alexgarnier78310@protonmail.com>
2026-04-03 11:43:54 +0200

    fix: update SMTP settings for action mailer to use port 465 and enable TLS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,11 +71,11 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:         "smtp.gmail.com",
-    port:            587,
+    port:            465,
     user_name:       Rails.application.credentials.dig(:smtp, :user_name),
     password:        Rails.application.credentials.dig(:smtp, :password),
     authentication:  "plain",
-    enable_starttls: true,
+    tls:             true,
     open_timeout:    10,
     read_timeout:    20
   }
