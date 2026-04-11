a3f104f9dcfb4039219f22b3199e547580555a07
alegarn <alexgarnier78310@protonmail.com>
2026-04-11 17:02:00 +0200

    Implement automatic client invite email toggle and update related settings

--- config/routes.rb
+++ config/routes.rb
@@ -70,6 +70,7 @@ Rails.application.routes.draw do
   get "settings/auth_setup", to: "settings#auth_setup", as: :auth_setup_settings
   patch "settings/auth_setup", to: "settings#complete_onboarding"
   patch "settings/crm_preferences", to: "settings#update_crm_preferences"
+  patch "settings/client_invitation_email/auto_send", to: "settings#update_invite_auto_send", as: :settings_invite_auto_send
   patch "settings/client_invitation_email", to: "settings#update_client_invitation_email", as: :settings_client_invitation_email
 
   get  "sign_in", to: "sessions#new"
